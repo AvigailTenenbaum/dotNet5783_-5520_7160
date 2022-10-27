6be9b9b9dfe9d3118e14c3f29ccbef5ca9bc41db
Avigail Tenenbaum <at6520764@gmail.com>
2022-10-27 19:37:41 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program5520.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome5520();
+            Welcome7160();
+            Console.ReadKey();
+        }
+        static partial void Welcome7160();
+        private static void Welcome5520()
+        {
+            string name;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
